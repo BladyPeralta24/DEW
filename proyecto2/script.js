d928defdddd6ca52d065789990386cc058d3c6fb
BladyPeralta24 <bladimir.peralta@iescesarmanrique.com>
2023-09-27 23:44:21 +0100

    en proceso...

--- proyecto2/script.js
+++ proyecto2/script.js
@@ -1,2 +1,11 @@
 /*Preguntar al usuario que pelicula quiere 
 elegir en una lista de peliculas*/
+document.write("<div class='container'>");
+document.write("<div id='film'>Pelicula 1</div>");
+document.write("<div id='film'>Pelicula 2</div>");
+document.write("<div id='film'>Pelicula 3</div>");
+document.write("</div>");
+
+
+
+var film = prompt("Choose one of the following movies :");
