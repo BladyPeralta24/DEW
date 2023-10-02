8f45138348059e17db0acc1cbe3a4b2fcf488343
BladyPeralta24 <bladimir.peralta@iescesarmanrique.com>
2023-10-03 00:12:30 +0100

    terminar el proyecto

--- proyecto2/script.js
+++ proyecto2/script.js
@@ -1,6 +1,5 @@
 
 
-
 // Crear un Objeto con su respetivo constructor
 
 function myCinema(rows,cols,movieName, ticketPrice, seatsHTML){
@@ -12,8 +11,14 @@ function myCinema(rows,cols,movieName, ticketPrice, seatsHTML){
   this.seatsHTML = seatsHTML; //Matriz para representar los asientos en HTML
 
   // Metodo para reservar un asiento
-  this.reserveSeat = function (row,col){
+  //this.bookingSeat = function (row,col){
     // Agregar logica de reserva aqui
+    /* si la casilla esta en blanco, entonces:*/
+  //};
+
+  // Metodo para liberar un asiento
+  this.freeingSeat = function  (row,col){
+
   };
 }
 
@@ -26,23 +31,37 @@ var film3 = new myCinema(10,14, "AVATAR", 9);
 
 
 
+function bookingSeat(){
+  if (list.includes(reserveSeat)){
+    console.log("el numero esta en la lista");
+  }else{
+    console.log("para ná");
+  }
+}
+
+
 // Lógica para generar una matriz para el número de butacas
 function createMatrix(rows, cols) {
-  var aux = 1;
+  //var aux = 1;
+  var matrix = [];
   var matrixHTML = '<table>';
   for (var i = 0; i < cols; i++) {
     matrixHTML += '<tr>';
+    matrix[i] = []; 
     for (var j = 0; j < rows; j++) {
       // Genera un número aleatorio entre 0 y 1 (0 o 1)
       var aleatorio = Math.floor(Math.random() * 2);
+      
 
       // Define un umbral para pintar la celda de rojo (por ejemplo, si el número aleatorio es 1)
       if (aleatorio === 1) {
-        matrixHTML += '<td class="red">' + aux + '</td>';
+        matrixHTML += '<td class="red">' + i+j + '</td>';
+        matrix[i][j] = 1;
       } else {
-        matrixHTML += '<td>' + aux + '</td>';
+        matrixHTML += '<td class="green">' + i+j + '</td>';
+        matrix[i][j] = 0;
       }
-      aux++;
+      // aux++;
     }
     matrixHTML += '</tr>';
   }
@@ -50,33 +69,75 @@ function createMatrix(rows, cols) {
   return matrixHTML;
 }
 
+
+
+
+function buySeats(){
+  var boolean = false;
+  while (boolean == false){
+    var reserveSeat = prompt("Select a seat to reserve: ");
+
+    // validar datos
+    while (isNaN(reserveSeat) || reserveSeat < 0 || reserveSeat % 1 !== 0){
+      alert ("ERROR. You must enter a positive.");
+      reserveSeat = prompt("Select a seat to reserve: ");
+      if (!isNaN(reserveSeat)){
+        reserveSeat = Number.isInteger(reserveSeat);
+      }else{
+        break;
+      }
+    }
+    console.log(reserveSeat);
+
+    var repeat = confirm("Do you want to continue Shopping");
+
+    if (repeat === true){
+      console.log("Continuar");
+    }else{
+      console.log("Cancelar");
+      boolean = true;
+    }
+  }
+  return reserveSeat;
+}
+
+
+
+
 // crear una funcion para pinchar en el cartel
 function showSeats(filmButton) {
   var matrixContainer = document.getElementById('seats');
-  var matrixHTML = '';
+
 
   switch (filmButton) {
     // implementar la lógica en el caso de que elija una de estas películas
     case 'film1':
       console.log('Salida con éxito');
       var seatsHTML = createMatrix(film1.rows,film1.cols);
-      matrixHTML = seatsHTML;
       break;
-    case 'film2':
-      console.log('Salida con éxito');
+      case 'film2':
+        console.log('Salida con éxito');
       var seatsHTML = createMatrix(film2.rows,film2.cols);
-      matrixHTML = seatsHTML;
       break;
     case 'film3':
       console.log('Salida con éxito');
       var seatsHTML = createMatrix(film3.rows,film3.cols);
-      matrixHTML = seatsHTML;
       break;
     default:
-      console.log('Nothing');
-  }
+        console.log('Nothing');
+      }
 
-  matrixContainer.innerHTML = matrixHTML;
+      matrixContainer.innerHTML = seatsHTML;
+      var buyButton = document.createElement("button");
+      buyButton.textContent = "Buy seats";
+      buyButton.onclick = function(){
+        buySeats();
+      };
+      var buyContainer = document.getElementById('buy');
+      buyContainer.innerHTML = "";
+      buyContainer.appendChild(buyButton);
+      // document.write("<button class='buttonBuy' onclick='buySeats()'>Buy</button>");
+  
 }
 
 
